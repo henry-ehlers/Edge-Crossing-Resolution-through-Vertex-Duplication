73031a3d05286c13e7722c76c6b6df444fa03962
Henry Ehlers <henry.ward.ehlers@gmail.com>
2022-08-04 10:24:18 +0200

    better loading and saving of graph data

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import networkx as nx
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     graph = create_barabasi_albert_graph(n=20, m=3, seed=1)
-
-    pos = nx.spring_layout(G=graph, iterations=5000, seed=24)
-    # pos = nx.kamada_kawai_layout(G=G)
+    print("here")
+    #pos = nx.spring_layout(G=graph, iterations=5000, seed=24)
+    pos = nx.kamada_kawai_layout(G=graph)
 
     # Color Map for both edges and vertices
     node_color_map = []
--- src/create_random_graphs.py
+++ src/create_random_graphs.py
@@ -1,10 +1,22 @@
 import networkx as nx
 import numpy as np
+from pathlib import Path
 
 
 def create_barabasi_albert_graph(n, m, seed=22, root_dir="data/simulated"):
+
+    #
     file_path = f = "./{}/barabasi_albert_{}_{}_{}.csv".format(root_dir, n, m, seed)
-    graph = nx.barabasi_albert_graph(n=n, m=m, seed=seed)
-    np.savetxt(fname=file_path, X=nx.to_numpy_matrix(graph).astype(int), fmt='%i', delimiter=",")
+
+    #
+    if Path(file_path).is_file():
+        data = np.loadtxt(fname=file_path, dtype=int, delimiter=",")
+        graph = nx.from_numpy_matrix(A=data)
+        print(graph)
+    else:
+        graph = nx.barabasi_albert_graph(n=n, m=m, seed=seed)
+        np.savetxt(fname=file_path, X=nx.to_numpy_matrix(graph).astype(int), fmt='%i', delimiter=",")\
+
+    #
     return graph
 
