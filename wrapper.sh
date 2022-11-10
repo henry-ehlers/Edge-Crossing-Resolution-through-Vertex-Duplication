64e8d437033a28f34a471c1ecd93b11206f0b18a
Henry Ehlers <henry.ward.ehlers@gmail.com>
2022-11-10 18:16:30 +0100

    starting the simulation run

--- wrapper.sh
+++ wrapper.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# Specify Hyper-arameters
 PROB=0.5
+
+# Specify new edge attachment parameter
 EDGES=4
 
-for NODES in 5
+# Iterate over first batch
+for NODES in 5 8 10 12 15
 do
-  echo "$NODES ------------------------------------------------------"
-  for SEED in $(seq 1 2)
+  echo "$NODES --------------------------------------------------------------------------------------------------"
+  for SEED in $(seq 1 50)
   do
       echo "$SEED"
       python main.py "$NODES" "$EDGES" "$SEED" > log/watts_strogatz_"$NODES"_"$EDGES"_"$SEED"_"$PROB".log
   done
 done
 
+# Specify new edge attachment parameter
+EDGES=6
+
+# Iterate over first batch
+for NODES in 8 10 12 15
+do
+  echo "$NODES --------------------------------------------------------------------------------------------------"
+  for SEED in $(seq 1 50)
+  do
+      echo "$SEED"
+      python main.py "$NODES" "$EDGES" "$SEED" > log/watts_strogatz_"$NODES"_"$EDGES"_"$SEED"_"$PROB".log
+  done
+done
